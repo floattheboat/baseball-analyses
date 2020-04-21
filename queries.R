181712ec888ed7f5dfff04ecd820be2e1ff39e4c
Matthew Johns <3mjohns@gmail.com>
2020-04-21 14:07:44 -0700

    completed connection to data tables

--- queries.R
+++ queries.R
@@ -1,3 +1,10 @@
 
 
 q_teams <- "SELECT yearID, lgID, teamID, name, divID, teamRank, WSWin, attendance FROM teams WHERE yearID >= 1985;"
+
+
+q_salaries <- "SELECT * FROM salaries;"
+
+
+q_postseason <- "SELECT * FROM seriespost WHERE round = 'WS' AND yearID >= 1985;"
+
